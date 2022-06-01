fb45bb1020e23a099b5360a4d63a16e6aeb6f025
Dmigit <dylanivanmolina@gmail.com>
2022-06-01 09:25:28 -0500

    finish setup

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log('Generated ispirational message!');
+const firstPart = ["You should never fear because ", "The sun always shines when ", "Turn that frown upside down because "];
+const secondPart = ["everything in life happens for a reason ", "you are you ", "life is beautiful "];
+const thirdPart = ["and all will be well in the end.", "and you'll reach your dreams faster than you think.", "and you are loved."];
